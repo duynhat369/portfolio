3c7528824b01ae5e8137429e121ff2eed74fbe60
Duy Nhat <duynhat719@gmail.com>
2021-04-22 00:38:27 +0700

    scrolling highlight  navbar

--- js/script.js
+++ js/script.js
@@ -3,22 +3,23 @@ const sections = document.querySelectorAll("section");
 const navLi = document.querySelectorAll(".navigator-items ul li a");
 
 window.addEventListener("scroll", () => {
-  let current = "";
+  let currentSection = ""; //id section
 
   sections.forEach((section) => {
+    //offsetTop là khoảng cách từ điểm tính tới đầu trang web
+    //pageYoffset là khoảng cách từ mép trên hiện tại của màn hình tới đầu trang web
     const sectionTop = section.offsetTop;
-    console.log(sectionTop);
     if (pageYOffset + 48 * 3 >= sectionTop) {
       /* - 3rem = 48px, vì 1 section có my-5 và pb-5 nên 48*3.
       - Mục đích để khi scroll hết nội dụng của section trên (vẫn còn padding và margin của nó) thì active section dưới
       - cần fix tiếp vì  có nhiều section có nội dung rất ngắn */
-      current = section.getAttribute("id");
+      currentSection = section.getAttribute("id");
     }
   });
 
   navLi.forEach((a) => {
     a.classList.remove("active");
-    if (a.classList.contains(current)) {
+    if (a.classList.contains(currentSection)) {
       //current là id section cũng là tên class của thẻ a
       a.classList.add("active");
     }
