9164de834019263c9b715aa31aa635fc2c11c59d
Duy Nhat <duynhat719@gmail.com>
2021-05-23 16:30:13 +0700

    comment code - No change code - Changed commit

--- js/script.js
+++ js/script.js
@@ -8,20 +8,21 @@ window.addEventListener("scroll", () => {
   sections.forEach((section) => {
     //offsetTop là khoảng cách từ điểm tính tới đầu trang web
     //pageYoffset là khoảng cách từ mép trên hiện tại của màn hình tới đầu trang web
+    /* - 3rem = 48px, vì 1 section có my-5 và p-5 nên 48*3.
+    - Mục đích để khi scroll hết nội dụng của section trên (vẫn còn padding và margin của nó) thì active section dưới
+    - cần fix tiếp vì  có nhiều section có nội dung rất ngắn */
     const sectionTop = section.offsetTop;
     if (pageYOffset + 48 * 3 >= sectionTop) {
-      /* - 3rem = 48px, vì 1 section có my-5 và p-5 nên 48*3.
-      - Mục đích để khi scroll hết nội dụng của section trên (vẫn còn padding và margin của nó) thì active section dưới
-      - cần fix tiếp vì  có nhiều section có nội dung rất ngắn */
       currentSection = section.getAttribute("id");
+      console.log(currentSection);
     }
   });
 
   navLi.forEach((a) => {
     a.classList.remove("active");
     if (a.classList.contains(currentSection)) {
-      //current là id section cũng là tên class của thẻ a
       a.classList.add("active");
     }
   });
 });
+// End script scroll to highlight navbar
